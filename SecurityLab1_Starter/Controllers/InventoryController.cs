67d6841296b3a9345e670f769c5a37dd2b80a33f
ekjoh <ekjohnson99@gmail.com>
2020-01-22 08:56:27 -0500

    Lab1 abc completed

--- SecurityLab1_Starter/Controllers/InventoryController.cs
+++ SecurityLab1_Starter/Controllers/InventoryController.cs
@@ -9,19 +9,27 @@ namespace SecurityLab1_Starter.Controllers
 {
     public class InventoryController : Controller
     {
+
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GenError()
+        {
+            throw new DivideByZeroException();
+            return View();
+        }
+
         protected override void OnException(ExceptionContext filterContext)
         {
-            
+
             filterContext.ExceptionHandled = true;
             //Log the error!!
             Logger.Log(filterContext.Exception.ToString());
             //Redirect or return a view, but not both.
             filterContext.Result = RedirectToAction("Index", "Error");
-        }
-        // GET: Inventory
-        public ActionResult Index()
-        {
-            return View();
         }
     }
 }
